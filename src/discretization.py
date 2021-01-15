c9c3e911697e2e7d17ddb32e83f725e41919cef9
adrien <adrien.corenflos@gmail.com>
2021-01-15 14:33:32 +0000

    Efficient discretization

--- src/discretization.py
+++ src/discretization.py
@@ -1,5 +1,19 @@
-from src.kalman.base import LGSSM
+from typing import Tuple
+
+import tensorflow as tf
+
 from src.kernels.base import ContinuousDiscreteModel
 
 
-def sde_to_lgssm(sde: ContinuousDiscreteModel) -> LGSSM:
+def sde_to_lgssm(sde: ContinuousDiscreteModel, dt: tf.Tensor) -> Tuple[tf.Tensor, ...]:
+    dtype = sde.F.dtype
+    A = tf.linalg.expm(sde.F * dt)
+    n = sde.F.shape[0]
+    Phi_high = tf.stack([sde.F,
+                         sde.L @ tf.linalg.matmul(sde.Q, sde.L, transpose_b=True)],
+                        axis=1)
+    Phi_low = tf.stack([tf.zeros((n, n), dtype=dtype), -tf.transpose(F)], axis=1)
+    Phi = tf.stack([Phi_high, Phi_low], axis=0)
+    AB = tf.linalg.expm(Phi * dt) @ tf.stack([tf.zeros((n, n), dtype=dtype), tf.eye(n, dtype)], axis=0)
+    Q = tf.linalg.matmul(AB[:n], A, transpose_b=True)
+    return A, Q
--- src/model.py
+++ src/model.py
@@ -1,24 +1,26 @@
+import gpflow
+from gpflow import Parameter
 from gpflow.kernels import Kernel
 from gpflow.models import GPModel
 from gpflow.models.model import MeanAndVariance
 from gpflow.models.training_mixins import InputData, RegressionData
-
-def _discretize_sde(sde_model, )
+from gpflow.models.util import data_input_to_tensor
 
 
 class StateSpaceGP(GPModel):
     def __init__(self,
-        data: RegressionData,
-        kernel: Kernel,
-        mean_function: Optional[MeanFunction] = None,
-        noise_variance: float = 1.0,
-    ):
+                 data: RegressionData,
+                 kernel: Kernel,
+                 noise_variance: float = 1.0,
+                 ):
         likelihood = gpflow.likelihoods.Gaussian(noise_variance)
+        self._noise_variance = Parameter(noise_variance)
         _, Y_data = data
-        super().__init__(kernel, likelihood, mean_function, num_latent_gps=Y_data.shape[-1])
+        super().__init__(kernel, likelihood, None, num_latent_gps=Y_data.shape[-1])
         self.data = data_input_to_tensor(data)
-    def _make_model(self, ts, ):
-    def predict_f(
-            self, Xnew: InputData, full_cov: bool = False, full_output_cov: bool = False
-    ) -> MeanAndVariance:
 
+    def _make_model(self, ts, ):
+
+        def predict_f(
+                self, Xnew: InputData, full_cov: bool = False, full_output_cov: bool = False
+        ) -> MeanAndVariance:
