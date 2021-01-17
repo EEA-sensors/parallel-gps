eb9eac4389d6b84a7d4a3e087975bd915a943fd2
zgbkdlm <zgbkdlm@gmail.com>
2021-01-17 15:09:48 +0200

    add toymodels and try gpflow

--- experiments/gp.py
+++ experiments/gp.py
@@ -1,4 +1,37 @@
 import numpy as np
 import gpflow as gpf
+import tensorflow as tf
 
-from ..src.toymodels import sinu
+from ..src.toymodels import sinu, obs_noise
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate data
+tf.random.set_seed(666)
+np.random.seed(666)
+
+t = np.linspace(0, 5, 500)
+ft = sinu(t)
+y = obs_noise(ft, 0.01*np.eye(t.shape[0]))
+
+# Init cov function
+cov_func = gpf.kernels.Matern32()
+
+# Init regression model
+m = gpf.models.GPR(data=(t, y), 
+                   kernel=cov_func, 
+                   mean_function=None)
+
+# Hyperparas
+m.likelihood.variance.assign(0.01)
+m.kernel.lengthscales.assign(0.5)
+
+# Hyperpara opt
+opt = gpf.optimizers.Scipy()
+opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(maxiter=100))
+
+# Prediction
+query = np.linspace(0, 5, 800)
+
+mean, var = m.predict_f(query)
--- src/toymodels/deter_funcs.py
+++ src/toymodels/deter_funcs.py
@@ -3,18 +3,77 @@ Some toy models based on deterministic test functions
 """
 import numpy as np
 
-def sinu():
-    pass
+from typing import Union
 
-def rect():
-    pass
+def sinu(t: np.ndarray) -> np.ndarray:
+    """
+    A sinusoidal test function. 
+    
+    y = sin(2 pi t) + sin(4 pi t) + cos(6 pi t) 
+    
+    Args:
+        t:  Input (time)
+    Return:
+        y:  y(t)
+    """
+    return np.sin(2 * np.pi * t) \
+            + np.sin(4 * np.pi * t) \
+            + np.cos(6 * np.pi * t)
+            
+
+def comp_sinu(t: np.ndarray) -> np.ndarray:
+    """
+    A composite sinusoidal test function. It is very
+    challenging for statitionary GP to model.
+    
+    y = sin^2(7 pi cos(2 pi t^2) t) / (cos(t pi t) + 2)
+    
+    Reference:
+        Deep State-space Gaussian processes. 2020
+    
+    Args:
+        t:  Input (time)
+    Return:
+        y:  y(t)
+    """
+    return np.sin(7 * np.pi * np.cos(2 * np.pi * (t ** 2))) ** 2 / \
+            (np.cos(5 * np.pi * t) + 2)
+            
+
+def rect(t: np.ndarray) -> np.ndarray:
+    """
+    A magnitude-varing rectangle signal. Very challenging
+    for a conventioanl GP to model.
+    
+    Reference:
+        Deep State-space Gaussian processes. 2020
+    
+    Args:
+        t:  Input (time)
+    Return:
+        y:  y(t)
+    """
+    # Scale to [0, 1]
+    tau = (t - np.min(t)) / (np.max(t) - np.min(t))
+    
+    # Jumping points
+    p = np.linspace(1/6, 5/6, 5)
+    
+    y = np.zeros(t.shape)
+    y[(tau >= 0) & (tau < p[0])] = 0
+    y[(tau >= p[0]) & (tau < p[1])] = 1
+    y[(tau >= p[1]) & (tau < p[2])] = 0
+    y[(tau >= p[2]) & (tau < p[3])] = 0.6
+    y[(tau >= p[3]) & (tau < p[4])] = 0
+    y[tau >= p[4]] = 0.4
+    
+    return y
+    
 
 def obs_noise(x: np.ndarray, 
-              H: np.ndarray, 
               R: np.ndarray) -> np.ndarray:
     """
-    Observe data x linearly with Gaussian noises. 
-    y = H x + r,   r ~ N(0, R)
+    Observe data x with Gaussian noises. 
+    y = x + r,   r ~ N(0, R)
     """
-    
-    return H @ x + np.linalg.cholesky(R) @ np.random.randn(x.shape)
+    return x + np.linalg.cholesky(R) @ np.random.randn(x.shape[0])
