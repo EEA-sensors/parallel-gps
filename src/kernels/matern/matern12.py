Wrong order for the Mixin, was hiding the init

src/kernels/matern/matern12.py
@@ -5,9 +5,8 @@ from src.kernels.base import ContinuousDiscreteModel, SDEKernelMixin
 from src.kernels.matern.common import get_matern_sde
 
 
-class Matern12(SDEKernelMixin, gpflow.kernels.Matern12):
+class Matern12(gpflow.kernels.Matern12, SDEKernelMixin):
     __doc__ = gpflow.kernels.Matern12.__doc__
-
     def get_sde(self) -> ContinuousDiscreteModel:
         F, L, Q, H = get_matern_sde(self.variance, self.lengthscales, 1)
         P_infty = tf.linalg.diag([self.variance])