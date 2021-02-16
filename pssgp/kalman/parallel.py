Fixed balancing of steady state covariance for Matern52

pssgp/kalman/parallel.py
@@ -113,6 +113,8 @@ def filtering_operator(elem1, elem2):
     eta = mv(temp, eta2 - mv(J2, b1), transpose_a=True) + eta1
     J = mm(temp, J2 @ A1, transpose_a=True) + J1
 
+    C = 0.5 * (C + tf.transpose(C, [0, 2, 1]))
+    J = 0.5 * (J + tf.transpose(J, [0, 2, 1]))
     return A, b, C, J, eta
 
 
@@ -154,23 +156,21 @@ def last_smoothing_element(m, P):
 def generic_smoothing_element(F, Q, m, P):
     Pp = F @ mm(P, F, transpose_b=True) + Q
     chol = tf.linalg.cholesky(Pp)
-    E = tf.transpose(tf.linalg.cholesky_solve(chol, F @ P))
+    E = tf.transpose(tf.linalg.cholesky_solve(chol, F @ P), [0, 2, 1])
     g = m - mv(E @ F, m)
     L = P - E @ mm(Pp, E, transpose_b=True)
+    L = 0.5 * (L + tf.transpose(L, [0, 2, 1]))
     return E, g, L
 
 
 def make_associative_smoothing_elements(Fs, Qs, filtering_means, filtering_covariances):
     last_elems = last_smoothing_element(filtering_means[-1], filtering_covariances[-1])
-    generic_elems = tf.vectorized_map(lambda z: generic_smoothing_element(*z),
-                                      (Fs[1:], Qs[1:], filtering_means[:-1], filtering_covariances[:-1]),
-                                      fallback_to_while_loop=False)
+    generic_elems = generic_smoothing_element(Fs[1:], Qs[1:], filtering_means[:-1], filtering_covariances[:-1])
     return tuple(tf.concat([gen_es, tf.expand_dims(last_e, 0)], axis=0)
                  for gen_es, last_e in zip(generic_elems, last_elems))
 
 
-def smoothing_operator(elems):
-    elem1, elem2 = elems
+def smoothing_operator(elem1, elem2):
     E1, g1, L1 = elem1
     E2, g2, L2 = elem2
 
@@ -187,10 +187,7 @@ def pks(lgssm, ms, Ps, max_parallel=10000):
     initial_elements = make_associative_smoothing_elements(Fs, Qs, ms, Ps)
     reversed_elements = tuple(tf.reverse(elem, axis=[0]) for elem in initial_elements)
 
-    def vectorized_operator(a, b):
-        return tf.vectorized_map(smoothing_operator, (a, b), fallback_to_while_loop=False)
-
-    final_elements = scan_associative(vectorized_operator,
+    final_elements = scan_associative(smoothing_operator,
                                       reversed_elements,
                                       max_num_levels=max_num_levels)
     return tf.reverse(final_elements[1], axis=[0]), tf.reverse(final_elements[2], axis=[0])

pssgp/kalman/sequential.py
@@ -18,11 +18,11 @@ def kf(lgssm, observations, return_loglikelihood=False, return_predicted=False):
         y, F, Q = inp
         mp = mv(F, m)
         Pp = F @ mm(P, F, transpose_b=True) + Q
+        Pp = 0.5 * (Pp + tf.transpose(Pp))
 
         def update(m, P, ell):
             S = H @ mm(P, H, transpose_b=True) + R
             yp = mv(H, m)
-
             chol = tf.linalg.cholesky(S)
             predicted_dist = MultivariateNormalTriL(yp, chol)
             ell_t = predicted_dist.log_prob(y)
@@ -36,6 +36,7 @@ def kf(lgssm, observations, return_loglikelihood=False, return_predicted=False):
         nan_y = ~tf.math.is_nan(y)
         nan_res = (ell, mp, Pp)
         ell, m, P = tf.cond(nan_y, lambda: update(mp, Pp, ell), lambda: nan_res)
+        P = 0.5 * (P + tf.transpose(P))
         return ell, m, P, mp, Pp
 
     ells, fms, fPs, mps, Pps = tf.scan(body,
@@ -57,7 +58,7 @@ def ks(lgssm, ms, Ps, mps, Pps):
         Ct = tf.linalg.cholesky_solve(chol, F @ P)
         sm = m + mv(Ct, sm - mp, transpose_a=True)
         sP = P + mm(Ct, sP - Pp, transpose_a=True) @ Ct
-        return sm, sP
+        return sm, 0.5 * (sP + tf.transpose(sP))
 
     (sms, sPs) = tf.scan(body,
                          (Fs[1:], Qs[1:], ms[:-1], Ps[:-1], mps[1:], Pps[1:]),

pssgp/kernels/matern/matern52.py
@@ -5,14 +5,14 @@ import tensorflow as tf
 
 from pssgp.kernels.base import ContinuousDiscreteModel, SDEKernelMixin, get_lssm_spec
 from pssgp.kernels.matern.common import get_matern_sde
-from pssgp.kernels.math_utils import balance_ss
+from pssgp.kernels.math_utils import balance_ss, solve_lyap_vec
 
 
 class Matern52(SDEKernelMixin, gpflow.kernels.Matern52):
     __doc__ = gpflow.kernels.Matern52.__doc__
 
     def __init__(self, variance=1.0, lengthscales=1.0, **kwargs):
-        self._balancing_iter = kwargs.pop('balancing_iter', 3)
+        self._balancing_iter = kwargs.pop('balancing_iter', 5)
         gpflow.kernels.Matern52.__init__(self, variance, lengthscales, **kwargs)
         SDEKernelMixin.__init__(self, **kwargs)
 
@@ -25,10 +25,10 @@ class Matern52(SDEKernelMixin, gpflow.kernels.Matern52):
         lamda = math.sqrt(5) / lengthscales
         variance = tf.reduce_sum(self.variance)
 
-        temp = lamda ** 2 * variance
-        P_infty = tf.linalg.diag([variance, temp / 3, temp ** 2])
-        P_infty = tf.tensor_scatter_nd_sub(P_infty, [[0, 2], [2, 0]], [temp / 3, temp / 3])
-
-        Fb, Lb, Hb, qb, Pb = balance_ss(F, L, H, q, P=P_infty, n_iter=self._balancing_iter)
-
-        return ContinuousDiscreteModel(Pb, Fb, Lb, Hb, qb)
+        # temp = lamda ** 2 * variance
+        # P_infty = tf.linalg.diag([variance, temp / 3, temp ** 2])
+        # P_infty = tf.tensor_scatter_nd_sub(P_infty, [[0, 2], [2, 0]], [temp / 3, temp / 3])
+        # tf.print(P_infty)
+        Fb, Lb, Hb, qb = balance_ss(F, L, H, q, n_iter=self._balancing_iter)
+        Pinf = solve_lyap_vec(Fb, Lb, qb)
+        return ContinuousDiscreteModel(Pinf, Fb, Lb, Hb, qb)

pssgp/kernels/math_utils.py
@@ -33,7 +33,6 @@ def balance_ss(F: tf.Tensor,
                L: tf.Tensor,
                H: tf.Tensor,
                q: tf.Tensor,
-               P: Optional[tf.Tensor] = None,
                n_iter: int = 5) -> Tuple[tf.Tensor, ...]:
     """Balance state-space model to have better numerical stability
 
@@ -83,9 +82,7 @@ def balance_ss(F: tf.Tensor,
     tmp4 = tf.reduce_max(tf.abs(H))
     H = H / tmp4
     q = (tmp4 ** 2) * q
-    if P is not None:
-        P = P * d[None, :] / d[:, None]
-    return F, L, H, q, d, P
+    return F, L, H, q
 
 
 def solve_lyap_vec(F: tf.Tensor,

tests/test_gp_vs_kfs.py
@@ -29,7 +29,7 @@ class GPEquivalenceTest(unittest.TestCase):
         self.K = 50
         self.t = np.sort(np.random.rand(self.T))
         self.ft = sinu(self.t)
-        self.y = obs_noise(self.ft, 0.1)
+        self.y = obs_noise(self.ft, 0.1, None)
         periodic_base = SquaredExponential(variance=1., lengthscales=0.5)
         self.covs = (
             (Matern12(variance=1., lengthscales=0.5), 1e-6, 1e-2),

tests/test_periodic.py
@@ -13,12 +13,12 @@ from pssgp.kernels.periodic import _get_offline_coeffs
 class PeriodicTest(unittest.TestCase):
 
     def setUp(self):
-        np.random.seed(31415926)
+        seed = 31415926
         self.T = 2000
         self.K = 800
         self.t = np.sort(np.random.rand(self.T))
         self.ft = sinu(self.t)
-        self.y = obs_noise(self.ft, 0.01)
+        self.y = obs_noise(self.ft, 0.01, seed)
 
         self.data = (tf.constant(self.t[:, None]), tf.constant(self.y[:, None]))
 